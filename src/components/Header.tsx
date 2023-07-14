1410f154b7c3c50ccdba33abe025df58a7b0bf9d
Phonedolly <Phonedolly@users.noreply.github.com>
2023-07-14 23:44:43 +0900

    uninstall framer-motion

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -389,20 +389,10 @@ export default function Header(props: {
         >
           {/* Menu Icon and Helper Text */}
           <div className="flex w-full flex-row items-center justify-between">
-            <AnimatePresence>
               {/* https://www.svgrepo.com/svg/510067/menu */}
               <div className="flex w-full flex-row items-center justify-start">
                 {writingPost === true ? (
-                  <motion.svg
-                    initial={{ opacity: 0 }}
-                    animate={{
-                      opacity: 1,
-                      transition: { ease: "linear", duration: 0.1 },
-                    }}
-                    exit={{
-                      opacity: 0,
-                      transition: { ease: "linear", duration: 0.5 },
-                    }}
+                  <svg
                     className="ml-3 h-9 w-9 animate-pulse p-1.5"
                     xmlns="http://www.w3.org/2000/svg"
                     width="800"
@@ -416,18 +406,9 @@ export default function Header(props: {
                       d="M22.801 1.749a1 1 0 00-1.18-.726C8.651 3.837 2.59 13.768.053 22.728a1 1 0 001.013 1.27A2.53 2.53 0 013.54 22l.288-.271a1 1 0 00-.766-1.726l.039-.1c.032-.046.06-.095.086-.147.205-.429.434-.673.664-.828.237-.159.538-.265.944-.323.414-.058.896-.061 1.479-.041.189.006.394.015.61.025.41.019.86.039 1.3.045 1.388.017 2.994-.096 4.662-.832 1.678-.74 3.334-2.07 4.903-4.351a1 1 0 00-.122-1.279c1.045-.592 1.964-1.393 2.722-2.29a11.732 11.732 0 002.213-4.013c.434-1.392.569-2.849.24-4.12zM6.343 16.565c-.585-.02-1.192-.024-1.77.052C7.637 10.661 12.741 5.35 20.984 3.23c.004.615-.104 1.31-.333 2.045A9.733 9.733 0 0118.82 8.59c-1.69 2.003-4.087 3.176-6.754 2.362a1 1 0 00-1.138 1.489c.643 1.022 1.942 1.722 3.591 1.796-.85.843-1.684 1.383-2.48 1.734-1.292.57-2.57.678-3.83.662a35.42 35.42 0 01-1.183-.04c-.224-.01-.45-.021-.683-.03z"
                       clipRule="evenodd"
                     ></path>
-                  </motion.svg>
+                  </svg>
                 ) : (
-                  <motion.svg
-                    initial={{ opacity: 0 }}
-                    animate={{
-                      opacity: 1,
-                      transition: { ease: "linear", duration: 0.1 },
-                    }}
-                    exit={{
-                      opacity: 0,
-                      transition: { ease: "linear", duration: 0.5 },
-                    }}
+                  <svg
                     onClick={() => setShowNavBar(true)}
                     className="ml-3 h-9 w-9 cursor-pointer p-1 transition-all duration-300 hover:scale-110 hover:transition"
                     xmlns="http://www.w3.org/2000/svg"
@@ -443,7 +424,7 @@ export default function Header(props: {
                       d="M4 5a1 1 0 000 2h16a1 1 0 100-2H4zm-1 7a1 1 0 011-1h16a1 1 0 110 2H4a1 1 0 01-1-1zm0 6a1 1 0 011-1h16a1 1 0 110 2H4a1 1 0 01-1-1z"
                       clipRule="evenodd"
                     ></path>
-                  </motion.svg>
+                  </svg>
                 )}
                 {writingPost === true ? (
                   <input
@@ -499,16 +480,10 @@ export default function Header(props: {
                   ></path>
                 </svg>
               </div>
-            </AnimatePresence>
           </div>
           {writingPost === true ? (
-            <AnimatePresence>
-              <motion.div
+              <div
                 className="flex h-[calc(100%-5rem)] w-full flex-row justify-center px-2 py-6 pt-2"
-                exit={{ opacity: 0 }}
-                transition={{
-                  opacity: { duration: 2 },
-                }}
               >
                 <Editor
                   className="relative -left-4 animate-enterance-from-top"
@@ -530,8 +505,7 @@ export default function Header(props: {
                     wordWrap: "on",
                   }}
                 />
-              </motion.div>
-            </AnimatePresence>
+              </div>
           ) : null}
         </div>
       </div>
